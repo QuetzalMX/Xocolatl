d96772a3353a4f85ab7828cc5bdd893ac9e027bb
Fernando Olivares <fernando.olivares@me.com>
2015-04-15 22:35:14 -0500

    Code style change to registering user.

--- Xocolatl/Auth/AuthRequestManager.m
+++ Xocolatl/Auth/AuthRequestManager.m
@@ -30,46 +30,32 @@ NSString *const UsersCollection = @"Users";
     manager.server = server;
     manager.connection = [database newConnection];
     
-    [[NSNotificationCenter defaultCenter] addObserver:manager
-                                             selector:@selector(yapDatabaseModified:)
-                                                 name:YapDatabaseModifiedNotification
-                                               object:database];
-    
     return manager;
 }
 
-- (void)registerUser:(NSString *)user
+- (void)registerUser:(NSString *)username
         withPassword:(NSString *)password
   andCompletionBlock:(void (^)(XOCUser *, NSError *))completionBlock;
 {
-    XOCUser *newUser = [XOCUser newUserWithUsername:user];
-    
+    __block XOCUser *newUser;
+    __block NSError *error;
     [self.connection readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
-        XOCUser *registeredUser = [transaction objectForKey:newUser.username
+        XOCUser *registeredUser = [transaction objectForKey:username
                                                inCollection:UsersCollection];
         if (registeredUser) {
-            completionBlock(nil, [NSError errorWithDomain:@"Account Creation"
-                                                     code:403
-                                                 userInfo:@{NSLocalizedDescriptionKey: @"Username already exists."}]);
-            return;
+            error = [NSError errorWithDomain:@"Account Creation"
+                                        code:403
+                                    userInfo:@{NSLocalizedDescriptionKey: @"Username already exists."}];
+        } else {
+            newUser = [XOCUser newUserWithUsername:username];
+            [newUser setHashedPassword:password];
+            [transaction setObject:newUser
+                            forKey:newUser.username
+                      inCollection:UsersCollection];
         }
-        
-        [newUser setHashedPassword:password];
-        [transaction setObject:newUser
-                        forKey:newUser.username
-                  inCollection:UsersCollection];
-        
-        transaction.yapDatabaseModifiedNotificationCustomObject = @{@"RegisteredUser": newUser,
-                                                                    @"completionBlock": completionBlock};
     }];
-}
-
-- (void)yapDatabaseModified:(NSNotification *)notification;
-{
-    NSDictionary *customObject = [notification.userInfo objectForKey:YapDatabaseCustomKey];
-    void (^completionBlock)(XOCUser *, NSError *) = customObject[@"completionBlock"];
-    XOCUser *user = customObject[@"RegisteredUser"];
-    completionBlock(user, nil);
+    
+    completionBlock(newUser, error);
 }
 
 @end
