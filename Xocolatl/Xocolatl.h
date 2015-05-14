f1a85ede93ddc2ee583ddd6ea884cf930da28e98
Fernando <fernando.olivares@me.com>
2015-05-14 12:52:28 -0500

    New header file added Xocolatl.h

---
+++ Xocolatl/Xocolatl.h
@@ -0,0 +1,43 @@
+//
+//  Xocolatl.h
+//  Xocolatl
+//
+//  Created by Cuenta on 5/14/15.
+//  Copyright (c) 2015 Quetzal. All rights reserved.
+//
+
+#ifndef Xocolatl_Xocolatl_h
+#define Xocolatl_Xocolatl_h
+
+#import "RoutingHTTPServer.h"
+#import "DatabaseResponder.h"
+#import "HTTPMessage+Xocolatl.h"
+#import "HTTPMessage.h"
+#import "HTTPResponse.h"
+#import "NSArray+transformativeArray.h"
+#import "NSData+RNSecureCompare.h"
+#import "NSString+randomString.h"
+#import "RNCryptor.h"
+#import "RNCryptorEngine.h"
+#import "RNDecryptor.h"
+#import "RNEncryptor.h"
+#import "RoutingResponder.h"
+#import "RoutingResponse.h"
+#import "SignInResponder.h"
+#import "SignUpResponder.h"
+#import "XOCUser+Auth.h"
+#import "XOCUser.h"
+#import "XOCUsersResponder.h"
+#import "XocolatlModelObject.h"
+#import "XocolatlJSONResponder.h"
+#import "XocolatlHTTPServer.h"
+#import "YapCollectionKey.h"
+#import "YapDatabase.h"
+#import "YapDatabaseConnection.h"
+#import "YapDatabaseExtension.h"
+#import "YapDatabaseExtensionConnection.h"
+#import "YapDatabaseExtensionTransaction.h"
+#import "YapDatabaseOptions.h"
+#import "YapDatabaseTransaction.h"
+
+#endif
